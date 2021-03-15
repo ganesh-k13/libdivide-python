8902097adc4cd2186bb50324e41ddb8e5b333285
Ganesh Kathiresan <ganesh3597@gmail.com>
2021-03-15 23:48:09 +0530

    BLD: Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup, Extension
+
+def main():
+    setup(name="read_list",
+          version="1.0.0",
+          description="Python interface for the read_list function",
+          author="Ganesh Kathiresan",
+          author_email="ganesh3597@gmail.com",
+          ext_modules=[Extension("read_list", ["divpy/floor_divide.c"])])
+
+if __name__ == "__main__":
+    main()
